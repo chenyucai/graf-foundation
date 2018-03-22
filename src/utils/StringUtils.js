4f25633d59359ded3b8372d78d01827d07791b0b
Nic Chan <e_ycchen@126.com>
2018-03-22 17:22:35 +0800

    add plusXing method

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -6,7 +6,16 @@ function toArray(string, char = ',') {
   return string.split(char);
 }
 
+function plusXing(str, frontLen, endLen, xingLen = 5) {
+  let xingStr = '';
+  for (let i = 0; i < xingLen; i++) {
+    xingStr += '*';
+  }
+  return str.substr(0, frontLen) + xingStr + str.substr(str.length - endLen);
+}
+
 export default {
   isBlank,
-  toArray
+  toArray,
+  plusXing
 }
